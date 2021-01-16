a5b60bef93cf42126201e6973b67013f1505d9c1
Norman <normanramadhan3@gmail.com>
2021-01-16 17:57:28 +0700

    Set up router

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,18 +1,42 @@
-import React, {useRef} from 'react'
-import {Card, Button, Form } from 'react-bootstrap'
+import React, {useRef, useState} from 'react'
+import {Card, Button, Form, Alert } from 'react-bootstrap'
+import {useAuth} from '../contexts/authContext'
 
 export default function Signup () {
   const emailRef = useRef()
   const passwordRef = useRef()
   const passwordConfirmRef = useRef()
+  const { signup } = useAuth()
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  async function handleSubmit (e) {
+    e.preventDefault()
+
+    if(passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError('Password do not match')
+    }
+
+    try {
+      setError('')
+      setLoading(true)
+      await signup (emailRef.current.value, passwordRef.current.value)
+    } catch {
+      setError('Failed to create an account')
+    }
+    setLoading(false)
+
+
+  }
 
 
   return (
     <>
       <Card>
         <Card.Body>
-          <h2 className="text-center mb-2">Sign Up</h2>
-          <Form>
+          <h2 className="text-center mb-2">Sign Up</h2>          
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
               <Form.Control type="email" ref={emailRef} required/>
@@ -25,7 +49,7 @@ export default function Signup () {
               <Form.Label>Password Confirmation</Form.Label>
               <Form.Control type="password" ref={passwordConfirmRef} required/>
             </Form.Group>       
-            <Button type="submit" className="w-100">Sign Up</Button>     
+            <Button disable={loading} type="submit" className="w-100">Sign Up</Button>     
           </Form>
         </Card.Body>
       </Card>
